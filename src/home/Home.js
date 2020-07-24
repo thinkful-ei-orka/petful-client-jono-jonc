0f03a966e85ca322e45998668f2eeb6a0291f101
Jon <jdog992@gmail.com>
2020-07-24 16:00:50 -0400

    added routes

---
+++ src/home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Home(props) {
+    return (
+        <div className='home-page'>
+            <p className='welcome-message'> Welcome to Petful! Our adoption process is a bit different. As you move to the 
+                adoption page and submit your name you will be added to the back of the line. You 
+                will see one dog and one cat on the screen. As a pet gets adopted they will be removed
+                from the screen and replaced with a new one. At this point you will be moved forward 
+                in the line. Once you reach the front of the line it is your turn to adopt! You will
+                have the option to adopt either animal you see or both!
+            </p>
+            <button className='get-started-button'>Get Started!</button>
+        </div>
+    )
+}
+
+export default Home;
--- src/root/Root.js
+++ src/root/Root.js
@@ -1,22 +1,35 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Route } from 'react-router-dom';
 import Cat from '../cat/Cat';
 import Dog from '../dog/Dog';
+import Home from '../home/Home';
 
-function Root() {
-  return (
-    <div>
-      <h1>Petful</h1>
-      <div className='adoption-line'>
+class Root extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Petful</h1>
+        <Route path='/' component={Home} exact/>
+        <Route path='/adoption' render={() => {
+          return (
+            <div>
+              
+              <div className='adoption-line'>
+              </div>
+              <div className='adoption'>
+                <Cat />
+                <Dog />
+              </div>
+              <h2 className='adopt-both'>Be a hero, adopt both!</h2>
+              <button className='adopt-both-button'>Adopt Both</button>
+            </div>
+          )
+        }
+        } />
       </div>
-      <div className='adoption'>
-        <Cat />
-        <Dog />
-      </div>
-      <h2 className='adopt-both'>Be a hero, adopt both!</h2>
-      <button className='adopt-both-button'>Adopt Both</button>
-    </div>
-  );
+    );
+  }
+
 };
 
 export default withRouter(Root);
