79d483dfe074dcdb2f72f6b1ce15f17db339e5cf
Jon <jdog992@gmail.com>
2020-07-24 18:35:11 -0400

    working on line

--- src/adoption/AdoptionLine.js
+++ src/adoption/AdoptionLine.js
@@ -6,26 +6,50 @@ export default class AdoptionList extends React.Component {
     isLoading: true,
     people: [],
   };
-  removePerson = () => {
-    if (this.state.people < 1) {
-      clearInterval(this.timerId);
-      console.log(`component unmounted`);
-    }
-    fetch(`${config.REACT_APP_API_ENDPOINT}/people`, {
-      method: 'DELETE',
-    }).then(() => {
-      this.setState({ people: this.state.people - 1 });
-    });
-  };
 
   componentDidMount = () => {
-    this.getPeople();
-    this.timerId = setInterval(this.removePerson(), 5000);
+    // this.getPeople();
+    this.timerId = setInterval(() => this.postPersonToLine(), 5000);
+    this.timerId2 = setInterval(() => this.removePerson(), 5000);
+    this.timerId3 = setInterval(() => this.getPeople(), 2000);
   };
+
+
   componentWillUnmount = () => {
     clearInterval(this.timerId);
     console.log(`component unmounted`);
   };
+
+  removePerson = () => {
+    // if (this.state.people < 1) {
+    //   clearInterval(this.timerId);
+    //   console.log(`component unmounted`);
+    // }
+    fetch(`${config.REACT_APP_API_ENDPOINT}/people`, {
+      method: 'DELETE',
+    }).then(() => {
+      this.setState({ isLoading: false });
+      this.getPeople()
+    });
+  };
+
+  postPersonToLine = () => {
+    let num = Math.floor(Math.random() * 50)
+    const newUser = {
+      name: `Test${num}`
+    }
+    const userString = JSON.stringify(newUser)
+    fetch(`${config.REACT_APP_API_ENDPOINT}/people`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: userString
+    })
+      .catch((error) => {
+        console.error({ error });
+      });
+  }
 
   getPeople = () => {
     fetch(`${config.REACT_APP_API_ENDPOINT}/people`)
@@ -41,8 +65,6 @@ export default class AdoptionList extends React.Component {
     if (this.state.isLoading) {
       return <div>loading...</div>;
     }
-    console.log(this.state);
-
     // while (this.state.people) {
     //   if (this.state.people <= 0) {
     //     clearTimeout(this.timerId);
