50b1994bc27bcc597d921dbcc09419f6b872a00d
Jon <jdog992@gmail.com>
2020-07-24 11:12:47 -0400

    small change

---
+++ config.js
@@ -0,0 +1,3 @@
+export default {
+    REACT_APP_API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || ''
+}
--- src/root/Root.js
+++ src/root/Root.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
 function Root() {
-  return <div>
+  return (
+  <div>
     <h1>Petful</h1>
+    <div className='cat-display'>
+      <h2>Cat</h2>
+      <p><strong>Name:</strong> Fluffy</p>
+      <p><strong>Gender:</strong> Female</p>
+      <p><strong>Age:</strong> 2 yrs</p>
+      <p><strong>Breed:</strong> Bengal</p>
+      <p><strong>Fluffy's story:</strong> Thrown on the street</p>
+      <button>Adopt</button>
+    </div>
   </div>
+  )
 }
 
 export default Root
